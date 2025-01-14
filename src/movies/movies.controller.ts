9ccfd167c96069030e2bae5dfcda90537c64c98a
Hyun ju <judoree9629@gmail.com>
2025-01-14 14:47:30 +0900

    Movies Service

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @Get()
+    getAll() {
+        return this.moviesService.getAll();
+    }
+    // @Get('serach')
+    // serach(@Query('year') serachingYear: string) {
+    //     return `We are searching for a movie made after ${serachingYear}`;
+    // }
+    @Get(':id')
+    getOne(@Param('id') movieId: string): Movie {
+        return this.moviesService.getOne(movieId);
+    }
+    @Post()
+    create(@Body() movieData) {
+        return this.moviesService.create(movieData);
+    }
+    @Delete(':id')
+    remove(@Param('id') movieId: string) {
+        return this.moviesService.deleteOne(movieId);
+    }
+    @Patch(':id')
+    pathch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updateData: movieId,
+            ...updateData,
+        };
+    }
+}
