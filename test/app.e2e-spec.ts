e631e8bb895b88d88fa7ad28bbef51d65f92b748
Hyun ju <judoree9629@gmail.com>
2025-01-15 15:20:32 +0900

    Testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
         app = moduleFixture.createNestApplication();
-        await app.init();
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+                forbidNonWhitelisted: true,
+                transform: true,
+            }),
+        );
     });
 
     it('/ (GET)', () => {
@@ -30,10 +36,20 @@ describe('AppController (e2e)', () => {
                     year: 2000,
                     genres: ['test'],
                 })
-                .expect(201);
+                .expect(200);
         });
         it('DELETE', () => {
             return request(app.getHttpServer()).delete('/movies').expect(404);
+        });
+        describe('/movies/:id', () => {
+            it('GET 200', () => {
+                return request(app.getHttpServer()).get('/movies/1').expect(200);
+            });
+            it('GET 404', () => {
+                return request(app.getHttpServer()).get('/movies/999').expect(404);
+            });
+            it.todo('DELETE');
+            it.todo('PATCH');
         });
     });
 });
